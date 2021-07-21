966909b9ec81eb1b5c8394f509b9ed1d6f0a926a
Jayadeep KM <kmjayadeep@gmail.com>
2021-07-21 12:11:05 +0530

    unmarshal data

--- main.go
+++ main.go
@@ -1,6 +1,35 @@
 package main
 
+import(
+  "fmt"
+  "os"
+  "io/ioutil"
+  "gopkg.in/yaml.v3"
+)
+
 
 func main() {
+  // Create index.html for output
+  out, err := os.Create("index.html")
+  defer out.Close()
+  check(err)
 
+  data := map[string]interface{}{}
+
+  file, err := ioutil.ReadFile("data.yaml")
+  check(err)
+
+  err = yaml.Unmarshal(file, &data)
+  check(err)
+
+  fmt.Println(data)
+
+}
+
+// check error and exit program
+func check(e error) {
+  if e != nil {
+    fmt.Println(e)
+    os.Exit(1)
+  }
 }
